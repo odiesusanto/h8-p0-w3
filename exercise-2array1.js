2f5e79c6d3b8f3da8bfb79f007a508167a250f87
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-03-06 18:15:31 +0700

    exercise-2 array 1

---
+++ exercise-2array1.js
@@ -0,0 +1,10 @@
+function balikString(word){
+  var string = '';
+  for(i = word.length - 1; i >= 0; i--){
+     string = string + word[i];
+  }
+  return string;
+}
+balikString("hello world!");
+
+console.log(balikString("hello world!"));
