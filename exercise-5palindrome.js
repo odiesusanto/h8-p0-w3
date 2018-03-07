d6a8fa03c01ed5ef5ace6514320dd2cd86fd9756
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-03-08 03:52:18 +0700

    exercise 5 palindrome

---
+++ exercise-5palindrome.js
@@ -0,0 +1,10 @@
+function palindrome(kata) {
+  var word = kata.length;
+  for(var i = 0; i < word / 2; i++){
+    if(kata[i] === kata[kata.length - 1]) {
+      return true;
+    } else {
+      return false;
+    }
+  }  
+}
