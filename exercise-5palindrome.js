4d32d275d3b90e9b9b87fe5e7d21a7c666e79066
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-03-10 03:05:13 +0700

    added console.log examples

--- exercise-5palindrome.js
+++ exercise-5palindrome.js
@@ -8,3 +8,19 @@ function palindrome(kata) {
     }
   }  
 } 
+function palindrome(kata) {
+  var word = kata.length;
+  for(var i = 0; i < word / 2; i++){
+    if(kata[i] === kata[kata.length - 1]) {
+      return true;
+    } else {
+      return false;
+    }
+  }  
+}
+console.log(palindrome('racecar'));
+console.log(palindrome('help'));
+console.log(palindrome('eye'));
+console.log(palindrome('kodok'));
+console.log(palindrome('swell'));
+console.log(palindrome('hannah'));
