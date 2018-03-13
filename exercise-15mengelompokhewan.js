4aa4c34be6af4d695372d6f4247abf0e44001f59
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-03-13 17:55:40 +0700

    exercise 15, renamed file

---
+++ exercise-15mengelompokhewan.js
@@ -0,0 +1,22 @@
+function groupAnimals(arr) {
+  var group = [];
+  
+  for (i=0; i < arr.length; i++) {
+    var list = [];
+    var animals = arr[i][0]; 
+    var sort = arr[i];
+    
+    for (var j=arr.length-1; j>i; j--) {
+      if (animals === arr[j][0]) {
+        sort += ', ' + arr[j];
+        arr.splice(j,1);
+      }
+    }
+    list.push(sort);
+    group.push(list);
+  }
+  return group.sort();
+}
+
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+//console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
