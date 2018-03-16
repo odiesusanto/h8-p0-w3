6cd4ac26d2b7b9317bb673248e35a309894e47b0
Odelia <odieawesome@Odelias-MacBook-Pro.local>
2018-03-17 00:42:03 +0700

    made correction/redo exercise

--- exercise-15mengelompokhewan.js
+++ exercise-15mengelompokhewan.js
@@ -1,22 +1,50 @@
+// function groupAnimals(arr) {
+//   var group = [];
+  
+//   for (i=0; i < arr.length; i++) {
+//     var list = [];
+//     var animals = arr[i][0]; 
+//     var sort = arr[i];
+    
+//     for (var j=arr.length-1; j>i; j--) {
+//       if (animals === arr[j][0]) {
+//         sort += ', ' + arr[j];
+//         arr.splice(j,1);
+//       }
+//     }
+//     list.push(sort);
+//     group.push(list);
+//   }
+//   return group.sort();
+// }
+
+// console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+//console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+
+
 function groupAnimals(arr) {
   var group = [];
   
-  for (i=0; i < arr.length; i++) {
-    var list = [];
-    var animals = arr[i][0]; 
-    var sort = arr[i];
-    
-    for (var j=arr.length-1; j>i; j--) {
-      if (animals === arr[j][0]) {
-        sort += ', ' + arr[j];
-        arr.splice(j,1);
-      }
+  arr.sort();
+  var list = arr[0][0];
+  var animals = [];
+  
+  for (i = 0; i < arr.length; i++) {
+   
+    if (list == arr[i][0]) {
+      group.push(arr[i]);
+    } else {
+      animals.push(group);
+      group = [];
+      list = arr[i][0];
+      group.push(arr[i]);
     }
-    list.push(sort);
-    group.push(list);
+    if (i === arr.length - 1) {
+      animals.push(group);
+    } 
   }
-  return group.sort();
+  return animals;
 }
 
 console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
-//console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
